bbb824eb4228551140f8b7fcb783c04a8901583f
Indiana Jonez <indiana.jonez@yandex.ru>
2023-07-24 18:10:46 +0300

    Initial Commit

---
+++ AvatarNative/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvatarNative
+//
+//  Created by Ekaterina Saveleva on 24.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
